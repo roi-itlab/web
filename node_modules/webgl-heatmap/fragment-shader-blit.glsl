44f512c1f89a12838ac4ac8f8e9547dde11c3f07
Danil9966 <danilikus17@gmail.com>
2017-03-13 19:01:59 +0300

    final

--- node_modules/webgl-heatmap/fragment-shader-blit.glsl
+++
@@ -1,9 +0,0 @@
-#ifdef GL_FRAGMENT_PRECISION_HIGH
-    precision highp int;
-    precision highp float;
-#else
-    precision mediump int;
-    precision mediump float;
-#endif
-uniform sampler2D source;
-varying vec2 texcoord;
